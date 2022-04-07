545c9c13139f7b464e138b3974a2eeb4f68c5f73
Patryk Szymik <patryk.szymik01@wp.pl>
2022-04-07 23:16:06 +0200

    Commit whole old code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+  const [vowelsAmount, setVowelsAmount] = useState(0);
+  const [charsAmount, setCharsAmount] = useState(0);
+  const [lettersAmount, setLettersAmount] = useState(0);
+  const [numbersAmount, setNumbersAmount] = useState(0);
+
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (text === "") {
+      alert("First type some text ;)");
+    } else {
+      setIsClicked(true);
+      let vowels = 0;
+      let chars = 0;
+      let letters = 0;
+      let numbers = 0;
+      let spaces = 0;
+      let string = text;
+      string = string.toLowerCase().split("");
+      string.map((letter, i) => {
+        if (
+          letter === "a" ||
+          letter === "e" ||
+          letter === "i" ||
+          letter === "o" ||
+          letter === "u" ||
+          letter === "ó" ||
+          letter === "ą" ||
+          letter === "ę" ||
+          letter === "y"
+        )
+          vowels = vowels + 1;
+        if (
+          letter !== "1" &&
+          letter !== "2" &&
+          letter !== "3" &&
+          letter !== "4" &&
+          letter !== "5" &&
+          letter !== "6" &&
+          letter !== "7" &&
+          letter !== "8" &&
+          letter !== "9" &&
+          letter !== "0"
+        )
+          letters = letters + 1;
+        if (letter === " ") spaces = spaces + 1;
+        chars = i;
+        return null;
+      });
+      letters = letters - spaces;
+      numbers = chars + 1 - spaces - letters;
+      setVowelsAmount(vowels);
+      setCharsAmount(chars + 1);
+      setLettersAmount(letters);
+      setNumbersAmount(numbers);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div className="welcome">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome on <strong>Text Analyzer!</strong>
+          <br />
+          Input your text below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="mainPage">
+        <input
+          type="text"
+          value={text}
+          placeholder="Type here..."
+          onChange={handleChangeText}
+          onClick={() => {
+            setText("");
+          }}
+        />
+        <button onClick={handleClick}>Analyze!</button>
+        {isClicked ? (
+          <Result
+            vowels={vowelsAmount}
+            chars={charsAmount}
+            letters={lettersAmount}
+            numbers={numbersAmount}
+          />
+        ) : null}
+      </div>
+      <div className="footer">
+        <p>Text Analyzer © 2021</p>
+      </div>
     </div>
   );
-}
+};
+
+const Result = (props) => {
+  return (
+    <div className="output">
+      <div className="analyze">
+        <p>
+          Liczba samogłosek: {props.vowels} <br />
+          Liczba znaków: {props.chars} <br />
+          Liczba liter: {props.letters} <br />
+          Liczba cyfr: {props.numbers} <br />
+        </p>
+      </div>
+      <div className="styles">
+        Liczba czasowników: <br />
+        Liczba rzeczowników: <br />
+        Liczba przymiotników: <br />
+      </div>
+    </div>
+  );
+};
 
 export default App;
